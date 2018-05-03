0265620757ba12d74be003e42bc69e6f08ff330f
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2018-05-03 16:06:33 +0200

    first two views

--- index.php
+++ index.php
@@ -4,19 +4,49 @@ require_once("vendor/autoload.php");
 
 $f3 = \Base::instance();
 
-class Main {
-    function render() {
-        echo 'Hello world';
+//globale Variablen in config.ini speichern
+$f3->config('config.ini');
+$f3->config('routes.ini');
+
+//moved into config.ini --- DEBUG LEVELS
+//$f3->set('DEBUG', 3);
+
+class AppController {
+    function beforeroute() {
+       // echo 'before routing for session - ';
     }
 
-    function sayhello() {
-        echo 'hallo babe';
+    function afterroute() {
+       // echo ' - After routing';
     }
 }
 
+class Main extends AppController {
 
-$f3->route('GET /','Main->render');
-$f3->route(GET /hello, 'Main->sayhello');
+    function render() {
+        $home = new Template;
+        echo $home->render('home.html');
+    }
+
+    function sayhello() {
+        $app = new Template;
+        echo $app->render('app.html');
+    }
+}
+
+class AboutPage extends AppController {
+    function render() {
+        echo 'This is the about page';
+    }
+}
+
+//moved into routes.ini
+//$f3->route('GET /','Main->render');
+//$f3->route('GET /test', 'Main->sayhello');
+//$f3->route('GET /about', 'AboutPage->render');
 
 $f3->run();
 
+//  php -S localhost:8088 -t .
+//function render($f3){
+//  echo $f3->get('variable'); }
