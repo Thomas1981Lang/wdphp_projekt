4e2628f2b65573af0e14e5433c9195b652666921
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2018-05-05 11:24:04 +0200

    Integrate database

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -1,10 +1,34 @@
 <?php
-class Controller {
-function beforeroute() {
-// echo 'before routing for session - ';
-}
 
-function afterroute() {
+class Controller
+{
+    protected $f3;
+    protected $db;
+
+    function beforeroute()
+    {
+// echo 'before routing for session - ';
+    }
+
+    function afterroute()
+    {
 // echo ' - After routing';
-}
+    }
+
+    function __construct()
+    {
+        $f3 = Base::instance();
+        $this->f3 = $f3;
+
+        $db = new DB\SQL(
+            $f3->get('devdb'),
+            $f3->get('devdbusername'),
+            $f3->get('devdbpassword'),
+            array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION)
+        );
+
+        $this->db = $db;
+    }
+
+
 }
---
+++ tmp/1rhmuqyegwskk.2vs0n00ra7swk.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SINGLES A LA CARTE</title>
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="/app/resources/css/reset.css">
+    <link rel="stylesheet" href="/app/resources/css/style.css">
+</head>
+<body>
+<header>
+
+    <div class="mobile_nav">
+        ddfgsfgsdfg
+    </div>
+
+
+    <div class="logo">
+        <h1 class="title">Singles a la carte</h1>
+        <h2 class="slogan">für Singles die wissen wo was passiert</h2>
+    </div>
+
+    <div class="nav_desktop"><span><a href="/" >Home</a></span><span><a href="/apps" class="active">App</a></span><span><a
+            href="">Sign In</a></span><span><a
+            href="">Login</a></span></div>
+
+
+    <div class="mobile_login">
+
+        sdfgsdg
+    </div>
+</header>
+
+<main class="app">
+
+
+    <!--    HEADER Section  -->
+    <section class="header">
+        <h1>Entdecke Singles in der Welt um dich herum</h1>
+        <a href="#">Download Singles to Go (Coming soon&excl;)</a>
+    </section>
+    <!--    MAIN Section    -->
+    <video src="/app/resources/videos/singletogo.mp4" autoplay loop>
+        Your browser does not support the video tag.
+    </video>
+
+    <h2>Dein eigener Reiseführer zu den heißesten Abenteuern</h2>
+    <p class="app_text">Singles to Go erinnert sich an Plätze und Singles die du magst und schlägt dir neue 'Points of Interest' in deiner Nähe vor.</p>
+
+    <p><img class="app_pic" src="/app/resources/img/aranxa-esteve-130749-unsplash.jpg" alt="Picture of a tent in the Desert"></p>
+
+    <p><img class="app_symbole" src="/app/resources/img/phone.png" alt="Smartphone Icon"></p>
+
+    <h2 class="app_slogan">Bald für iPhone und Android erhältlich</h2>
+    <a href="#">Download Singles to Go (Coming soon&excl;)</a>
+
+
+</main>
+
+<footer>
+    <p><a href="https://github.com/Thomas1981Lang" target="_blank">&copy; TL </a></p>
+    <p><span><a href="#">Impressum</a></span><span><a href="#">Nutzungsbedingung</a></span><span><a
+            href="#">Datenschutz</a></span></p>
+</footer>
+
+</body>
+</html>
