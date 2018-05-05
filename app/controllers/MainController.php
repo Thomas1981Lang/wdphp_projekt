0e4de821591d56561aa6be47b31cbfad3500a54b
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2018-05-05 13:22:13 +0200

    add database testsite

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -11,4 +11,19 @@ class MainController extends Controller {
         $app = new Template;
         echo $app->render('app.html');
     }
+
+    function test() {
+
+        $messages = new Messages($this->db);
+        $messages->key = 'Secondmessage';
+        $messages->message = 'This is the second message inserted from code.';
+        $messages->save();
+
+        $messages = new Messages($this->db);
+        $msg = $messages->all()[1];
+
+        $this->f3->set('msg', $msg);
+        $template = new Template;
+        echo $template->render('test.html');
+    }
 }
